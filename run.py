c1c8dd5cb0f6a17cb5f7fbf97738ba15f63a1a02
DARKEST-OWL-HACKING-SOCIETY <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-06-07 12:11:52 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+import os, sys
+
+try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
+
+except:pass
+
+try:
+    __import__("windows_enc").Main()
+
+except Exception as e:
+
+    exit(str(e))
